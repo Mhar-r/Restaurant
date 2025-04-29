9c81187dbcdd85c71fbf4b420a55943208bf6b57
Mhar-r <lopezgarciamharyorimariel@gmail.com>
2025-04-28 19:18:02 -0600

    Ignorar config.ts para evitar conflictos de IP

---
+++ E-commerce/config.example.ts
@@ -0,0 +1,2 @@
+// config.example.ts
+export const API_URL = "http://tu-ip-aqui:8000";
--- E-commerce/config.ts
+++
@@ -1,4 +0,0 @@
-export const CONFIG = {
-    BASE_URL: "http://192.168.0.105:8000",  // Cambia a tu IP Johannson
-  };
-  
